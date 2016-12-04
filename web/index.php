6a46fde07e990f35e3034038028c48580073202a
Hynnej <hackworj@oregonstate.edu>
2016-12-03 17:17:25 -0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -17,7 +17,8 @@
 	
 	if($method == "POST")
 	{
-		 $datas = json_decode(file_get_contents("php://input"), true);
+		 $data = json_decode(file_get_contents("php://input"), true);
+		var_dump($data);
 	}
 
 	if(doc == "users")
