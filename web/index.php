2cb6f242bf0cfe718a7168496934bb18e12ae0b4
Hynnej <hackworj@oregonstate.edu>
2016-12-04 19:51:30 -0700

    Add files via upload

--- web/index.php
+++ web/index.php
@@ -119,7 +119,7 @@
 				$addPlace = array(
 				'gId' => $data['gId'],
 				'category' => $data['category'],	
-			   	'name' => $data['name'],
+			    'name' => $data['name'],
 				'address' => $data['address'],		
 				'rating' => $data['rating'],
 				'comments' => $data['comments']);
@@ -148,15 +148,12 @@
 		else if(empty($place))
 		{
 			$query = array('gId' => $gId);
-
-			$users->deleteOne(array('gId' => $gId));
-				
-				$data = array("response" => "User Info and Places Deleted");
-				header('Content-type: application/json');
-				echo json_encode((object)($data));
 			
-
-				
+			$users->deleteOne($query);
+			$places->deleteMany($query);
+			$data = array("response" => "User Info and Places Deleted");
+			header('Content-type: application/json');
+			echo json_encode((object)($data));					
 		}
 	}
 	
