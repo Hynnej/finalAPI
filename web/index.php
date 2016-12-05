f55507051533c2171994750414090c8fffcb3dcc
Hynnej <hackworj@oregonstate.edu>
2016-12-04 19:44:33 -0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -149,7 +149,7 @@
 		{
 			$query = array('gId' => $gId);
 
-			$users->removearray('gId' => $gId));
+			$users->remove(array('gId' => $gId));
 				
 				$data = array("response" => "User Info and Places Deleted");
 				header('Content-type: application/json');
